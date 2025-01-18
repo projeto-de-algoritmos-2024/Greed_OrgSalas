0c473ad0855b11ccdb1298962025f9d989d51af4
Pedro <pedrohenben2014@gmail.com>
2025-01-17 23:02:20 -0300

    Add verifica conflito

--- teste.js
+++ teste.js
@@ -1,11 +1,29 @@
-class Aula {
-    constructor(nome, horarioInicio, horarioFim) {
-        this.nome = nome;
-        this.horarioInicio = horarioInicio;
-        this.horarioFim = horarioFim;
-        this.sala = null;
+class SistemaAgendamento {
+    constructor() {
+        this.aulas = [];
+        this.salas = [];
+    }
+
+    horarioParaMinutos(horario) {
+        const [horas, minutos] = horario.split(':').map(Number);
+        return horas * 60 + minutos;
+    }
+
+    temConflito(aula1, aula2) {
+        const inicio1 = this.horarioParaMinutos(aula1.horarioInicio);
+        const fim1 = this.horarioParaMinutos(aula1.horarioFim);
+        const inicio2 = this.horarioParaMinutos(aula2.horarioInicio);
+        const fim2 = this.horarioParaMinutos(aula2.horarioFim);
+
+        return !(fim1 <= inicio2 || fim2 <= inicio1);
     }
 }
 
-const testeAula = new Aula("Matemática", "10:00", "12:00");
-console.log(testeAula);
+module.exports = SistemaAgendamento;
+
+const sistema = new SistemaAgendamento();
+console.log(sistema.horarioParaMinutos("10:30"));
+
+const aula1 = { horarioInicio: "10:00", horarioFim: "12:00" };
+const aula2 = { horarioInicio: "11:00", horarioFim: "13:00" };
+console.log(sistema.temConflito(aula1, aula2)); 
